0971698249ef5ec85b5ecc5e2ef5be1b1493fdb0
Benedikt Franke <benedikt.franke@mll.com>
2022-05-16 09:13:23 +0200

    Support `illuminate/support` version 9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## v1.2.0
+
+### Added
+
+- Support `illuminate/support` version 9
+
 ## v1.1.1
 
 ### Fixed
--- rector.php
+++ rector.php
@@ -1,17 +1,14 @@
 <?php declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Concat\JoinStringConcatRector;
-use Rector\Core\Configuration\Option;
+use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::PHP_71);
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(SetList::CODE_QUALITY);
+    $rectorConfig->import(SetList::PHP_71);
 
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::SKIP, [
+    $rectorConfig->skip([
         // skip csv test file to keep `\r` and `\n` for readability
         JoinStringConcatRector::class => [
             // single file
@@ -20,8 +17,8 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     ]);
 
     // paths to refactor; solid alternative to CLI arguments
-    $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests']);
+    $rectorConfig->paths([__DIR__ . '/src', __DIR__ . '/tests']);
 
-    // Path to phpstan with extensions, that PHPSTan in Rector uses to determine types
-    $parameters->set(Option::PHPSTAN_FOR_RECTOR_PATH, getcwd() . '/phpstan-for-config.neon');
+    // Path to PHPStan with extensions, that PHPStan in Rector uses to determine types
+    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
 };
