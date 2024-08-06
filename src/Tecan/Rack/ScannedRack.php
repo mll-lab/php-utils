16da2370f9afc8426554e4963fdfa0a240af70a5
Simon Bigelmayr <simon.bigelmayr@mll.com>
2024-08-06 09:21:17 +0200

    comment ScannedRack.php

--- src/Tecan/Rack/ScannedRack.php
+++ src/Tecan/Rack/ScannedRack.php
@@ -1,5 +1,8 @@
 <?php declare(strict_types=1);
 
 namespace MLL\Utils\Tecan\Rack;
-
+/**
+ * Interface for racks that have been scanned and the barcode of the tubes is known.
+ * For example, @see FluidXRack.
+ */
 interface ScannedRack extends Rack {}
