1473c0e19379021e653bc2bef7acbd6f841a1dd0
Simon Bigelmayr <simon.bigelmayr@mll.com>
2024-08-06 09:31:24 +0200

    data provider for repetitive tests

--- src/Tecan/Rack/ScannedRack.php
+++ src/Tecan/Rack/ScannedRack.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 
 namespace MLL\Utils\Tecan\Rack;
+
 /**
  * Interface for racks that have been scanned and the barcode of the tubes is known.
  * For example, @see FluidXRack.
--- tests/Tecan/Rack/RackTest.php
+++ tests/Tecan/Rack/RackTest.php
@@ -3,6 +3,7 @@
 namespace MLL\Utils\Tests\Tecan\Rack;
 
 use MLL\Utils\Tecan\Rack\AlublockA;
+use MLL\Utils\Tecan\Rack\BaseRack;
 use MLL\Utils\Tecan\Rack\DestLC;
 use MLL\Utils\Tecan\Rack\DestPCR;
 use MLL\Utils\Tecan\Rack\DestTaqMan;
@@ -13,6 +14,7 @@ use MLL\Utils\Tecan\Rack\MPCDNA;
 use MLL\Utils\Tecan\Rack\MPSample;
 use MLL\Utils\Tecan\Rack\MPWater;
 use MLL\Utils\Tecan\Rack\NoEmptyPositionOnRack;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 final class RackTest extends TestCase
@@ -60,84 +62,27 @@ final class RackTest extends TestCase
         $rack->assignPosition('Sample', $lastPosition + 1);
     }
 
-    public function testRackA(): void
+    /** @return iterable<string, array{BaseRack, string, string, int}> */
+    public static function rackDataProvider(): iterable
     {
-        $rack = new AlublockA();
-        self::assertSame('A', $rack->name());
-        self::assertSame('Eppis 24x0.5 ml Cooled', $rack->type());
-        self::assertSame(24, $rack->positionCount());
-        self::assertCount(24, $rack->positions);
+        yield 'MPCDNA' => [new MPCDNA(), 'MPCDNA', 'MP cDNA', 96];
+        yield 'MPSample' => [new MPSample(), 'MPSample', 'MP Microplate', 96];
+        yield 'FluidXRack' => [new FluidXRack(), 'FluidX', '96FluidX', 96];
+        yield 'DestLC' => [new DestLC(), 'DestLC', '96 Well MP LightCycler480', 96];
+        yield 'DestPCR' => [new DestPCR(), 'DestPCR', '96 Well PCR ABI semi-skirted', 96];
+        yield 'DestTaqMan' => [new DestTaqMan(), 'DestTaqMan', '96 Well PCR TaqMan', 96];
+        yield 'MasterMixRack' => [new MasterMixRack(), 'MM', 'Eppis 32x1.5 ml Cooled', 32];
+        yield 'AlublockA' => [new AlublockA(), 'A', 'Eppis 24x0.5 ml Cooled', 24];
+        yield 'MPWater' => [new MPWater(), 'MPWasser', 'Trough 300ml MCA Portrait', 1];
     }
 
-    public function testRackMPCDNA(): void
+    /** @dataProvider rackDataProvider */
+    #[DataProvider('rackDataProvider')]
+    public function testRackProperties(BaseRack $rack, string $expectedName, string $expectedType, int $expectedPositionCount): void
     {
-        $rack = new MPCDNA();
-        self::assertSame('MPCDNA', $rack->name());
-        self::assertSame('MP cDNA', $rack->type());
-        self::assertSame(96, $rack->positionCount());
-        self::assertCount(96, $rack->positions);
-    }
-
-    public function testRackMPSample(): void
-    {
-        $rack = new MPSample();
-        self::assertSame('MPSample', $rack->name());
-        self::assertSame('MP Microplate', $rack->type());
-        self::assertSame(96, $rack->positionCount());
-        self::assertCount(96, $rack->positions);
-    }
-
-    public function testRackMPWasser(): void
-    {
-        $rack = new MPWater();
-        self::assertSame('MPWasser', $rack->name());
-        self::assertSame('Trough 300ml MCA Portrait', $rack->type());
-        self::assertSame(1, $rack->positionCount());
-        self::assertCount(1, $rack->positions);
-    }
-
-    public function testRackFluidX(): void
-    {
-        $rack = new FluidXRack();
-        self::assertSame('FluidX', $rack->name());
-        self::assertSame('96FluidX', $rack->type());
-        self::assertSame(96, $rack->positionCount());
-        self::assertCount(96, $rack->positions);
-    }
-
-    public function testRackMM(): void
-    {
-        $rack = new MasterMixRack();
-        self::assertSame('MM', $rack->name());
-        self::assertSame('Eppis 32x1.5 ml Cooled', $rack->type());
-        self::assertSame(32, $rack->positionCount());
-        self::assertCount(32, $rack->positions);
-    }
-
-    public function testRackDestLC(): void
-    {
-        $rack = new DestLC();
-        self::assertSame('DestLC', $rack->name());
-        self::assertSame('96 Well MP LightCycler480', $rack->type());
-        self::assertSame(96, $rack->positionCount());
-        self::assertCount(96, $rack->positions);
-    }
-
-    public function testRackDestPCR(): void
-    {
-        $rack = new DestPCR();
-        self::assertSame('DestPCR', $rack->name());
-        self::assertSame('96 Well PCR ABI semi-skirted', $rack->type());
-        self::assertSame(96, $rack->positionCount());
-        self::assertCount(96, $rack->positions);
-    }
-
-    public function testRackDestTaqMan(): void
-    {
-        $rack = new DestTaqMan();
-        self::assertSame('DestTaqMan', $rack->name());
-        self::assertSame('96 Well PCR TaqMan', $rack->type());
-        self::assertSame(96, $rack->positionCount());
-        self::assertCount(96, $rack->positions);
+        self::assertSame($expectedName, $rack->name());
+        self::assertSame($expectedType, $rack->type());
+        self::assertSame($expectedPositionCount, $rack->positionCount());
+        self::assertCount($expectedPositionCount, $rack->positions);
     }
 }
