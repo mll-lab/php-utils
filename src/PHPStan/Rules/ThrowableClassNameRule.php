2932ef92117f799e18351d250603f8fbd4f77638
Benedikt Franke <benedikt.franke@mll.com>
2024-11-14 17:06:17 +0100

    review

--- src/PHPStan/Rules/ThrowableClassNameRule.php
+++ src/PHPStan/Rules/ThrowableClassNameRule.php
@@ -2,15 +2,14 @@
 
 namespace MLL\Utils\PHPStan\Rules;
 
+use Illuminate\Support\Str;
 use PhpParser\Node;
 use PhpParser\Node\Stmt\Class_;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleErrorBuilder;
 
-/**
- * @implements Rule<Class_>
- */
+/** @implements Rule<Class_> */
 class ThrowableClassNameRule implements Rule
 {
     public function getNodeType(): string
@@ -20,30 +19,35 @@ class ThrowableClassNameRule implements Rule
 
     public function processNode(Node $node, Scope $scope): array
     {
-        $className = (string) $node->name;
+        $nodeName = $node->name;
+        if ($nodeName === null) {
+            return [];
+        }
 
-        if ($node->extends === null) {
-            if (str_ends_with($className, 'Exception')) {
+        $className = $nodeName->name;
+
+        $extends = $node->extends;
+        if ($extends === null) {
+            if (Str::endsWith($className, 'Exception')) {
                 return [
-                    RuleErrorBuilder::message(sprintf(
-                        'Class "%s" uses the suffix "Exception" but does not extend \Throwable. Consider using "Exemption" or another term.',
-                        $className
-                    ))->build(),
+                    RuleErrorBuilder::message(<<<TXT
+                    Class "{$className}" is suffixed with "Exception" but does not extend \Exception. Consider using "Exemption" or another term.
+                    TXT)->build(),
                 ];
             }
 
             return [];
         }
 
-        $parentClass = $node->extends->toString();
-        $extendsThrowable = is_subclass_of($parentClass, \Throwable::class) || $parentClass === \Throwable::class;
+        $parentClass = $extends->toString();
+        $extendsThrowable = is_subclass_of($parentClass, \Exception::class)
+            || $parentClass === \Exception::class;
 
-        if ($extendsThrowable && ! str_ends_with($className, 'Exception')) {
+        if ($extendsThrowable && ! Str::endsWith($className, 'Exception')) {
             return [
-                RuleErrorBuilder::message(sprintf(
-                    'Class "%s" extends \Throwable but does not use the suffix "Exception".',
-                    $className
-                ))->build(),
+                RuleErrorBuilder::message(<<<TXT
+                Class "{$className}" extends \Exception but is not suffixed with "Exception".
+                TXT)->build(),
             ];
         }
 
