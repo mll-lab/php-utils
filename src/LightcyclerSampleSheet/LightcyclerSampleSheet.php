1ed9b145120fe6b7180bf3c4270393789405a951
Simon Bigelmayr <simon.bigelmayr@mll.com>
2024-12-18 10:10:18 +0100

    simplify

--- src/LightcyclerSampleSheet/LightcyclerSampleSheet.php
+++ src/LightcyclerSampleSheet/LightcyclerSampleSheet.php
@@ -10,28 +10,29 @@ class LightcyclerSampleSheet
 {
     private const WINDOWS_NEW_LINE = "\r\n";
     private const TAB_SEPARATOR = "\t";
+    public const HEADER_COLUMNS = [
+        '"General:Pos"',
+        '"General:Sample Name"',
+        '"General:Repl. Of"',
+        '"General:Filt. Comb."',
+        '"Sample Preferences:Color"',
+    ];
 
     /** @param Microplate<SampleDTO, CoordinateSystem12x8> $microplate */
     public function generate(Microplate $microplate): string
     {
-        $sampleSheet = implode(self::TAB_SEPARATOR, [
-            '"General:Pos"',
-            '"General:Sample Name"',
-            '"General:Repl. Of"',
-            '"General:Filt. Comb."',
-            '"Sample Preferences:Color"',
-        ]) . self::WINDOWS_NEW_LINE;
+        $sampleSheet = implode(self::TAB_SEPARATOR, self::HEADER_COLUMNS) . self::WINDOWS_NEW_LINE;
 
         foreach ($microplate->filledWells() as $coordinateFromKey => $well) {
             $replicationOf = $well->replicationOf instanceof Coordinates ? $well->replicationOf->toString() : '';
-
-            $sampleSheet .= implode(self::TAB_SEPARATOR, [
+            $row = [
                 Coordinates::fromString($coordinateFromKey, $microplate->coordinateSystem)->toString(),
                 "\"{$well->sampleName}\"",
                 "\"{$replicationOf}\"",
                 $well->filterCombination,
                 "$00{$well->hexColor}",
-            ]) . self::WINDOWS_NEW_LINE;
+            ];
+            $sampleSheet .= implode(self::TAB_SEPARATOR, $row) . self::WINDOWS_NEW_LINE;
         }
 
         return $sampleSheet;
