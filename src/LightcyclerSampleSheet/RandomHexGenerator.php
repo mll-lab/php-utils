bd2bc33e54676b875de1b6fed235a395da2dbc94
Simon Bigelmayr <simon.bigelmayr@mll.com>
2025-01-09 10:51:42 +0100

    - hex6Digits to uniqueHex6Digits - test coverage

--- src/LightcyclerSampleSheet/RandomHexGenerator.php
+++ src/LightcyclerSampleSheet/RandomHexGenerator.php
@@ -4,12 +4,20 @@ namespace MLL\Utils\LightcyclerSampleSheet;
 
 class RandomHexGenerator
 {
-    public function hex6Digits(): string
-    {
-        $randomNumber = mt_rand(0, 0xFFFFFF);
-        $hexString = dechex($randomNumber);
-        $paddedHexString = str_pad($hexString, 6, '0', STR_PAD_LEFT);
+    /** @var list<string> */
+    private array $generatedHexCodes = [];
 
-        return strtoupper($paddedHexString);
+    public function uniqueHex6Digits(): string
+    {
+        do {
+            $randomNumber = mt_rand(0, 0xFFFFFF);
+            $hexString = dechex($randomNumber);
+            $paddedHexString = str_pad($hexString, 6, '0', STR_PAD_LEFT);
+            $uniqueHexCode = strtoupper($paddedHexString);
+        } while (in_array($uniqueHexCode, $this->generatedHexCodes, true));
+
+        $this->generatedHexCodes[] = $uniqueHexCode;
+
+        return $uniqueHexCode;
     }
 }
--- tests/LightcyclerSampleSheet/RelativeQuantificationSheetTest.php
+++ tests/LightcyclerSampleSheet/RelativeQuantificationSheetTest.php
@@ -5,6 +5,8 @@ namespace MLL\Utils\Tests\LightcyclerSampleSheet;
 use Illuminate\Support\Collection;
 use MLL\Utils\LightcyclerSampleSheet\RelativeQuantificationSample;
 use MLL\Utils\LightcyclerSampleSheet\RelativeQuantificationSheet;
+use MLL\Utils\Microplate\Coordinates;
+use MLL\Utils\Microplate\CoordinateSystem12x8;
 use MLL\Utils\StringUtil;
 use PHPUnit\Framework\TestCase;
 
@@ -17,7 +19,7 @@ final class RelativeQuantificationSheetTest extends TestCase
             'B1' => new RelativeQuantificationSample('Sample 2', '498-640', '4899D1', null),
             'C1' => new RelativeQuantificationSample('Sample 3', '498-640', '8528B9', null),
             'D1' => new RelativeQuantificationSample('Sample 4', '498-640', '8E05D9', null),
-            'E1' => new RelativeQuantificationSample('Sample 5', '498-640', '4080A5', null),
+            'E1' => new RelativeQuantificationSample('Sample 5', '498-640', '4080A5', Coordinates::fromString('C12', new CoordinateSystem12x8())),
         ]);
 
         $sheet = new RelativeQuantificationSheet();
@@ -29,7 +31,7 @@ A1\t"Sample 1"\t""\t498-640\t$00FF378A
 B1\t"Sample 2"\t""\t498-640\t$004899D1
 C1\t"Sample 3"\t""\t498-640\t$008528B9
 D1\t"Sample 4"\t""\t498-640\t$008E05D9
-E1\t"Sample 5"\t""\t498-640\t$004080A5
+E1\t"Sample 5"\t"C12"\t498-640\t$004080A5
 
 EOT;
 
