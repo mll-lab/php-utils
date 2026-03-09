f20f9678505c8781de4ed7bb8af22aace7991fa9
Simon Bigelmayr <s.bigelmayr@gmx.net>
2026-03-09 11:32:58 +0100

    feat: add MemoryMonitor utility

---
+++ src/MemoryMonitor.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1);
+
+namespace MLL\Utils;
+
+use function Safe\ini_get;
+
+class MemoryMonitor
+{
+    private const UNIT_FACTOR = 1024;
+
+    /** @var array<int, string> */
+    private const UNITS = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+    public static function usage(): int
+    {
+        return memory_get_usage();
+    }
+
+    public static function peakUsage(): int
+    {
+        return memory_get_peak_usage();
+    }
+
+    public static function availableMemory(): int
+    {
+        $memoryLimit = ini_get('memory_limit');
+        if ($memoryLimit === '-1') {
+            return -1;
+        }
+
+        return self::convertToBytes($memoryLimit);
+    }
+
+    public static function formattedUsage(?string $context = null): string
+    {
+        $current = self::formatBytes(self::usage());
+        $available = self::availableMemory();
+        $availableFormatted = $available === -1
+            ? 'Unlimited'
+            : self::formatBytes($available);
+
+        $usage = "Memory Usage: {$current} / {$availableFormatted}";
+
+        if ($context !== null) {
+            return "{$context} - {$usage}";
+        }
+
+        return $usage;
+    }
+
+    public static function convertToBytes(string $memoryLimit): int
+    {
+        $unit = strtoupper($memoryLimit[strlen($memoryLimit) - 1]);
+
+        switch ($unit) {
+            case 'G':
+                $factor = self::UNIT_FACTOR ** 3;
+                break;
+            case 'M':
+                $factor = self::UNIT_FACTOR ** 2;
+                break;
+            case 'K':
+                $factor = self::UNIT_FACTOR;
+                break;
+            default:
+                throw new \UnexpectedValueException("Unexpected unit value: {$unit}.");
+        }
+
+        return (int) $memoryLimit * $factor;
+    }
+
+    public static function formatBytes(int $bytes): string
+    {
+        $bytes = max($bytes, 0);
+        $pow = (int) floor(($bytes > 0 ? log($bytes) : 0) / log(self::UNIT_FACTOR));
+        $pow = min($pow, count(self::UNITS) - 1);
+
+        return round($bytes / (self::UNIT_FACTOR ** $pow)) . ' ' . self::UNITS[$pow];
+    }
+}
